0f551248e14d30351ad2efe0b2b7e5a5b0b2971e
DoliScriptz <132125366+DoliScriptz@users.noreply.github.com>
2025-06-25 00:23:22 +0700

    Update loader.js

--- api/script/loader.js
+++ api/script/loader.js
@@ -1,7 +1,7 @@
-export default (req, res) => {
-  if (req.method !== 'GET') return res.status(405).end()
-  const lua = `
-local H=game:GetService'HttpService'local P=game.Players.LocalPlayer
+export default (req,res)=>{
+  if(req.method!=='GET')return res.status(405).end()
+  const lua=`
+local H=game:GetService'HttpService';local P=game.Players.LocalPlayer
 local R=(syn and syn.request)or(http and http.request)or(request)or(http_request)assert(R)
 local I=R{Url='https://makalhub.vercel.app/api/init',Method='POST',
   Headers={['Content-Type']='application/json'},
@@ -15,5 +15,5 @@ local S=R{Url='https://makalhub.vercel.app/api/script/'..N,Method='POST',
 assert(S and S.Body)
 loadstring(S.Body)()`
   res.setHeader('Content-Type','text/plain')
-  res.status(200).send(lua.trim())
+  res.send(lua.trim())
 }
