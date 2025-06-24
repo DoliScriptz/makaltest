5be023442b34784d43a43eb19a482fd62bb11daf
DoliScriptz <132125366+DoliScriptz@users.noreply.github.com>
2025-06-25 00:27:27 +0700

    Update loader.js

--- api/script/loader.js
+++ api/script/loader.js
@@ -1,19 +1,20 @@
-export default (req,res)=>{
-  if(req.method!=='GET')return res.status(405).end()
+export default (req,res) => {
+  if (req.method!=='GET') return res.status(405).end()
   const lua=`
-local H=game:GetService'HttpService';local P=game.Players.LocalPlayer
-local R=(syn and syn.request)or(http and http.request)or(request)or(http_request)assert(R)
-local I=R{Url='https://makalhub.vercel.app/api/init',Method='POST',
-  Headers={['Content-Type']='application/json'},
+local H=game:GetService"HttpService" local P=game.Players.LocalPlayer
+local R=(syn and syn.request)or(http and http.request)or(request)or(http_request) assert(R)
+local I=R{Url="https://makalhub.vercel.app/api/init",Method="POST",
+  Headers={["Content-Type"]="application/json"},
   Body=H:JSONEncode{userid=P.UserId,username=P.Name}}
+assert(I and I.Body)
 local T=H:JSONDecode(I.Body).token
-local M={[537413528]='babft',[109983668079237]='stealabrainrot',[18687417158]='forsaken'}
-local N=M[game.PlaceId]assert(N)
-local S=R{Url='https://makalhub.vercel.app/api/script/'..N,Method='POST',
-  Headers={['Content-Type']='application/json'},
+local M={[537413528]="babft",[109983668079237]="stealabrainrot",[18687417158]="forsaken"}
+local N=M[game.PlaceId] assert(N)
+local S=R{Url="https://makalhub.vercel.app/api/script/"..N,Method="POST",
+  Headers={["Content-Type"]="application/json"},
   Body=H:JSONEncode{name=N,token=T}}
 assert(S and S.Body)
 loadstring(S.Body)()`
   res.setHeader('Content-Type','text/plain')
-  res.send(lua.trim())
+  res.status(200).send(lua.trim())
 }
