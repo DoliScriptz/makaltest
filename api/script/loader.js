ba8ce49ce71d17a049b35096936998a330711ee4
DoliScriptz <132125366+DoliScriptz@users.noreply.github.com>
2025-06-25 00:10:14 +0700

    Update loader.js

--- api/script/loader.js
+++ api/script/loader.js
@@ -3,13 +3,17 @@ export default (req,res)=>{
   const lua=`
 local H=game:GetService'HttpService'local P=game.Players.LocalPlayer
 local R=(syn and syn.request)or(http and http.request)or(request)or(http_request)assert(R)
-local I=R{Url='https://makalhub.vercel.app/api/init',Method='POST',Headers={['Content-Type']='application/json'},Body=H:JSONEncode{userid=P.UserId,username=P.Name}}
+local I=R{Url='https://makalhub.vercel.app/api/init',Method='POST',
+  Headers={['Content-Type']='application/json'},
+  Body=H:JSONEncode{userid=P.UserId,username=P.Name}}
 local T=H:JSONDecode(I.Body).token
 local M={[537413528]='babft',[109983668079237]='stealabrainrot',[18687417158]='forsaken'}
 local N=M[game.PlaceId]assert(N)
-local S=R{Url='https://makalhub.vercel.app/api/script/'..N,Method='POST',Headers={['Content-Type']='application/json'},Body=H:JSONEncode{name=N,token=T}}
+local S=R{Url='https://makalhub.vercel.app/api/script/'..N,Method='POST',
+  Headers={['Content-Type']='application/json'},
+  Body=H:JSONEncode{name=N,token=T}}
 assert(S and S.Body)
 loadstring(S.Body)()`
   res.setHeader('Content-Type','text/plain')
-  res.send(lua)
+  res.send(lua.trim())
 }
